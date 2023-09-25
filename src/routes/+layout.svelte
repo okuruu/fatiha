454e6bd31ff9d856ce8b76916e919220e1eceb9b
okuruu <Phantom_Ghost@outlook.co.id>
2023-09-25 23:01:37 +0700

    Starting Journey 💫

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import '../app.css';
+</script>
+
+<div class="container mx-auto">
+    <div class="navbar bg-base-100">
+        <div class="navbar-start">
+            <div class="dropdown">
+            <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+            <label tabindex="0" for="navigationBar" class="btn btn-ghost btn-circle">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+            </label>
+            <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+            <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <li><a href="/">Belajar Al-Qur'an</a></li>
+                <li><a href="/">Tafsir & Hadits</a></li>
+                <li><a href="/about">Tentang</a></li>
+            </ul>
+            </div>
+        </div>
+        <div class="navbar-center">
+            <a href="/" class="btn btn-ghost normal-case text-xl">Fatiha</a>
+        </div>
+        <div class="navbar-end">
+            <button class="btn btn-ghost btn-circle">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+            </button>
+        </div>
+    </div>
+</div>
+
+<slot/>
